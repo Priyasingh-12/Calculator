4d31344587d161bf121800a2a3283ade6b973a29
priya singh <piyasngh12@gmail.com>
2025-09-12 17:15:04 +0530

    add hover and javascript code

--- operate.js
+++ operate.js
@@ -1,1 +1,15 @@
 let inputBox = document.getElementById("inputbox") ;
+let buttons =document.querySelectorAll("button") ;
+let string = ' ' ;
+
+buttons.forEach(element => {
+    element.addEventListener("click" , (b) => {
+        console.log(b); 
+
+        if(b.target.innerText == '='){
+            string =  String(eval(string))
+            inputBox.value = string ;
+        }
+
+    })
+})
